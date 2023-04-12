bc14f03d995a585b1eea57c27ef756f22f00703c
ochiru wa <ochiruwa@gmail.com>
2023-04-12 21:24:13 +0000

    Upload New File

---
+++ webserver.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+from threading import Thread
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"]) 
+def home():
+  return render_template('index.html')
+def run():
+  app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
